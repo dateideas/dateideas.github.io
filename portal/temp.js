4d47beef6c027ff3d2176de7b8490edc99c2b0a0
Metta Ong <ongspxm@gmail.com>
2018-05-30 12:18:40 +0800

    init commit

---
+++ portal/temp.js
@@ -0,0 +1,42 @@
+Vue.component("temp_ticket", {
+    template: document.getElementById("v_temp_tickets"),
+    data: function(){
+        return {
+            code: "loading...",
+            points: "loading...",
+            showing: true,
+            overlay: false,
+            refferal: "",
+        };
+    },
+    methods:{
+        toggleShow: function(){
+            if(this.showing){ this.showing=false; }
+            else{ this.showing=true; }
+        },
+        refer: function(){
+            this.overlay = false;
+            api.send("/temp/referral?code="+this.refferal,
+                obj => {
+                    alert("referral code submitted");
+                    window.location.reload();
+                },
+                err => {
+                    alert("unable to submit referral code");
+                }
+            );
+        }
+    },
+    created: function(){
+        api.send("/temp/info", obj => {
+            this.code = obj.code;
+            this.points = obj.points;
+        }); 
+
+        api.send("/temp/newUser", obj => {
+            if(obj.newUser){
+                this.overlay = true;
+            }
+        });
+    }
+});
